9f2f0207327d12b20883368ab00e698887f7de4c
jhwanlee8@gmail.com <jhwanlee8@gmail.com>
2019-01-15 20:58:27 -0500

    Implementing analyzeText

--- WordFrequencyAnalyzer/WordFrequencyAnalyzer.cpp
+++ WordFrequencyAnalyzer/WordFrequencyAnalyzer.cpp
@@ -9,9 +9,12 @@ std::unordered_set<std::string> stopWords;
 
 int initializeStopWords();
 void analyzeText(bool includeStopWords);
+std::string findRootWord(std::string original);
 
 int main() {
-	initializeStopWords();
+	if (initializeStopWords() == -1) {
+		std::cout << "Please make sure that stopWords.txt file is present in the directory\n";
+	}
 	int choice = -1;
 	std::cout << "WordFrequency Analyzer\n";
 	bool includeStopWords = false;
@@ -48,7 +51,7 @@ int main() {
 
 int initializeStopWords() {
 	std::ifstream stopWordsStream;
-	stopWordsStream.open("stopWords");
+	stopWordsStream.open("stopWords.txt");
 	if (!stopWordsStream.is_open()) {
 		return -1;
 	}
@@ -61,5 +64,39 @@ int initializeStopWords() {
 }
 
 void analyzeText(bool includeStopWords) {
+	std::string filename;
+	std::unordered_map<std::string, unsigned int> count_map; //Hash map that counts each root word
+	std::cout << "Enter TXT Filename: ";
+	std::cin >> filename;
+	std::ifstream txtFileStream;
+	txtFileStream.open(filename.c_str());
+	if (!txtFileStream.is_open()) {
+		std::cout << "TXT Filename not found\n";
+		return;
+	}
+	std::string temp = "";
+	while (txtFileStream >> temp) {
+		if (!includeStopWords) {
+			if (stopWords.find(temp) != stopWords.end()) { // this word is a stop word
+				continue;
+			}
+		}
 
+		std::string rootWord = findRootWord(temp);
+		if (count_map.find(rootWord) == count_map.end()) { //This word hasn't been encountered before
+			count_map[rootWord] = 1;
+		}
+		else {
+			count_map[rootWord]++;
+		}
+	}
+	
+	//Sort by value
+	//Print out top 25
+	//Save the Analysis and update the log
+}
+
+std::string findRootWord(std::string original) {
+	//Implement root word extraction algorithm
+	return "";
 }
